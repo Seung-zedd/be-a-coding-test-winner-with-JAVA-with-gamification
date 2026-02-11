39b2a99e7c1db0356f2d68872e133cd74bd152fb
michael Jo <csz0209@gmail.com>
2026-02-11 14:05:39 +0900

    ⚙️chore: migrate java version to 11 to match on coding test env

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,6 @@
-
-import array.*;
-import stack.DecimalBinarySolution;
-import stack.DeleteWithPairSolution;
-import stack.StockPriceSolution;
-import stack.ValidParenthesisLeftRotateSolution;
-
-import java.util.Arrays;
-
+// Main.java를 단순히 버전 확인용으로 바꾸고 싶다면:
 public class Main {
     public static void main(String[] args) {
-        StockPriceSolution s = new StockPriceSolution();
-        System.out.println(Arrays.toString(s.solution(new int[]{1, 2, 3, 2, 3})));
+        System.out.println("Current Java Version: " + System.getProperty("java.version"));
     }
 }
