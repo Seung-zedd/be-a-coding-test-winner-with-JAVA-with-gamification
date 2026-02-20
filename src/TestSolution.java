755ba52c7d1978a4474ea05c5d48c14845c8544e
michael Jo <csz0209@gmail.com>
2026-02-20 14:59:04 +0900

    ⚔️퀘스트 완료: 행렬의 곱셈 던전 완전 정복!
    desc: 메인 클래스도 완벽한 호환을 위해 자바 11로 컴파일 수정

--- src/TestSolution.java
+++ src/TestSolution.java
@@ -1,10 +1,31 @@
-/*
- * n^2 Array Slicing Problem 
- */
+import array.MatrixMulSolution;
+import java.util.Arrays;
 
 public class TestSolution {
-    public int[] solution(int N, int[] stages) {
-        int[] answer = {};
-        return answer;
+    public static void main(String[] args) {
+        MatrixMulSolution solver = new MatrixMulSolution();
+
+        // Test Case 1
+        int[][] arr1 = { { 1, 4 }, { 3, 2 }, { 4, 1 } };
+        int[][] arr2 = { { 3, 3 }, { 3, 3 } };
+        int[][] expected1 = { { 15, 15 }, { 15, 15 }, { 15, 15 } };
+        int[][] result1 = solver.solution(arr1, arr2);
+        printResult(1, expected1, result1);
+
+        // Test Case 2
+        int[][] arr3 = { { 2, 3, 2 }, { 4, 2, 4 }, { 3, 1, 4 } };
+        int[][] arr4 = { { 5, 4, 3 }, { 2, 4, 1 }, { 3, 1, 1 } };
+        int[][] expected2 = { { 22, 22, 11 }, { 36, 28, 18 }, { 29, 20, 14 } };
+        int[][] result2 = solver.solution(arr3, arr4);
+        printResult(2, expected2, result2);
+    }
+
+    private static void printResult(int n, int[][] expected, int[][] result) {
+        boolean match = Arrays.deepEquals(expected, result);
+        System.out.println("Test Case " + n + ": " + (match ? "PASS" : "FAIL"));
+        if (!match) {
+            System.out.println("  Expected: " + Arrays.deepToString(expected));
+            System.out.println("  Actual:   " + Arrays.deepToString(result));
+        }
     }
 }
--- src/array/MatrixMulSolution.java
+++ src/array/MatrixMulSolution.java
@@ -1,32 +1,23 @@
 package array;
 
-/*
-1. 10^2 * 10^2 = 10^4 < 10^8이므로 완전탐색 가능!
-
-<코드 설계>
-1. 행벡터와 열벡터를 각각 저장할 크기 100인 리스트를 초기화(1차원 배열은 원소 1개만 저장 가능하므로)
-2. 각각의 벡터를 (x, y) 좌표로 치환해서 생각
-    2.1. Coordinate라는 커스텀 클래스 생성 필요
-
-
-
-
- */
-
-
 public class MatrixMulSolution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        // 1. 행렬 곱의 size = (m x k) x (k x n) = m x n
+        // 1. 각 row와 col을 추출
         int r1 = arr1.length;
         int c1 = arr1[0].length;
         int c2 = arr2[0].length;
-        int[][] answer = new int[r1][c2]; // 리턴할 정답 배열
 
+        // 2. answer의 크기를 구한다
+        int[][] answer = new int[r1][c2];
+
+        // 3. 행렬의 곱셈을 실행해서 answer에 값을 채워넣음
+        // arr1의 row를 순회해야할 i, arr2의 row를 순회해야할 j(왜냐하면 2차원 배열은 row를 기준으로 밖에 순회를 못하기 때문)
+        // 행렬의 곱을 수행하기 위해 추가적인 인덱스 k가 필요함
+        // 시간복잡도: O(10^2 * 10^2 * 10^2) = O(10^6), O.K!
         for (int i = 0; i < r1; i++) {
             for (int j = 0; j < c2; j++) {
-                // 내부 차원에서의 성분 곱
                 for (int k = 0; k < c1; k++) {
-                    answer[i][j] += arr1[i][k] * arr2[k][j];
+                    answer[i][j] += (arr1[i][k] * arr2[k][j]);
                 }
             }
         }
