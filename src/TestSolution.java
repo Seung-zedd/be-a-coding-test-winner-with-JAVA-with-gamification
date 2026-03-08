46274b3fedfc5e27768409e6fe9a4cd03d31f6ed
michael Jo <csz0209@gmail.com>
2026-03-08 13:15:58 +0900

    👾일일 퀘스트 완료: 요세푸스 순열 4회차 정복(S랭크)

--- src/TestSolution.java
+++ src/TestSolution.java
@@ -1,34 +1,37 @@
 import queue.FunDevSolution;
-import queue.CardDeckSolution;
-import stack.TableEditSolution;
+import queue.JosephusSolution;
 import hash.MakeSumSolution;
 import java.util.Arrays;
 
 public class TestSolution {
     public static void main(String[] args) {
-        // [복습] Table Edit Quest (Lv. 2 Review: 3회차)
-        // testTableEdit();
+        // [복습] Josephus Problem Quest (Lv. 3 Review: 3회차)
+        testJosephus();
 
         // [복습] Function Development Quest (Lv. 1 Review: 1회차)
-        testFunDev();
+        // testFunDev();
 
         // [신규] Make Sum Quest (Quest 18: Hash)
         // testMakeSum();
-
-        // [복습] Card Deck Quest (Lv. 1 Review: 1회차)
-        // testCardDeck();
     }
 
-    private static void testTableEdit() {
-        TableEditSolution solver = new TableEditSolution();
-        System.out.println("\n--- [복습] (Table Edit) ---");
-        int n = 8;
-        int k = 2;
-        String[] cmd = { "D 2", "C", "U 3", "C", "D 4", "C", "U 2", "Z", "Z" };
-        String expected = "OOOOXOOO";
-        String result = solver.solution(n, k, cmd);
-        System.out.println("Test Case 1: " + (expected.equals(result) ? "PASS" : "FAIL"));
-        System.out.println("Result: " + result);
+    private static void testJosephus() {
+        JosephusSolution solver = new JosephusSolution();
+        System.out.println("\n--- [복습] (Josephus Problem) ---");
+
+        // Case 1
+        int n1 = 5, k1 = 2;
+        int[] exp1 = { 2, 4, 1, 5, 3 };
+        int[] res1 = solver.solution(n1, k1);
+        System.out.println("Test Case 1: " + (Arrays.equals(exp1, res1) ? "PASS" : "FAIL"));
+        System.out.println("Result: " + Arrays.toString(res1));
+
+        // Case 2
+        int n2 = 7, k2 = 3;
+        int[] exp2 = { 3, 6, 2, 7, 5, 1, 4 };
+        int[] res2 = solver.solution(n2, k2);
+        System.out.println("Test Case 2: " + (Arrays.equals(exp2, res2) ? "PASS" : "FAIL"));
+        System.out.println("Result: " + Arrays.toString(res2));
     }
 
     private static void testFunDev() {
@@ -69,26 +72,5 @@ public class TestSolution {
         boolean res2 = solver.solution(arr2, target2);
         System.out.println("Test Case 2: " + (res2 == false ? "PASS" : "FAIL"));
         System.out.println("Result: " + res2);
-    }
-
-    private static void testCardDeck() {
-        CardDeckSolution solver = new CardDeckSolution();
-        System.out.println("\n--- [복습] (Card Deck) ---");
-
-        // Case 1
-        String[] c1_1 = { "i", "drink", "water" };
-        String[] c2_1 = { "want", "to" };
-        String[] g1 = { "i", "want", "to", "drink", "water" };
-        String result1 = solver.solution(c1_1, c2_1, g1);
-        System.out.println("Test Case 1: " + ("Yes".equals(result1) ? "PASS" : "FAIL"));
-        System.out.println("Result: " + result1);
-
-        // Case 2
-        String[] c1_2 = { "i", "water", "drink" };
-        String[] c2_2 = { "want", "to" };
-        String[] g2 = { "i", "want", "to", "drink", "water" };
-        String result2 = solver.solution(c1_2, c2_2, g2);
-        System.out.println("Test Case 2: " + ("No".equals(result2) ? "PASS" : "FAIL"));
-        System.out.println("Result: " + result2);
     }
 }
