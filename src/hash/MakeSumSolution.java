3620d7fed9601c950814cde4b655ed40324012bb
michael Jo <csz0209@gmail.com>
2026-03-10 15:02:06 +0900

    ⚔️퀘스트 완료: 두 개의 수로 특정값 만들기 (Make Sum) 던전  정복(A랭크)
    desc: Deque의 사용 용도를 파악하지 못함 && 어떨 때 set에 데이터를 저장해야 하는지 논리를 제대로 파악하지 못함 -> 자료구조는 과거의 역할을 하기 때문에 현재의 데이터를 저장하는 용도로 사용

--- src/hash/MakeSumSolution.java
+++ src/hash/MakeSumSolution.java
@@ -9,6 +9,8 @@ import java.util.*;
 n개의 양의 정수로 이루어진 리스트 arr와 정수 target이 주어졌을 때, 
 arr에 있는 정수 중에서 서로 다른 두 수를 더해 target을 만들 수 있는지 
 여부를 반환하는 solution() 함수를 작성하세요.
+-> 서로 다른 두 수를 더해 target을 만들 수 있는가? == 보수의 접근법을 활용
+즉, num + x = target, x = target - num으로 현재 순회하고 있는 데이터를 num으로 설정한다.
 
 [입력]
 - arr: n개의 양의 정수로 이루어진 배열
@@ -18,14 +20,25 @@ arr에 있는 정수 중에서 서로 다른 두 수를 더해 target을 만들 
 - target을 만들 수 있으면 true, 없으면 false
 
 [제한 사항]
-- n은 2 이상 10,000 이하의 자연수입니다.
+- n은 2 이상 10^4 이하의 자연수입니다.
 - arr의 각 원소는 1 이상 1,000,000 이하의 자연수입니다.
 - target은 1 이상 2,000,000 이하의 자연수입니다.
 */
 
 public class MakeSumSolution {
     public boolean solution(int[] arr, int target) {
-        // [용사님의 로직을 기다리고 있습니다]
+        // 스택 아니면 큐 -> 덱으로 풀 수 있을 것 같은데? 왜냐하면 target만 만족하는 데이터만 가져오면 되는거 아님?
+        // ! 덱은 front 또는 rear에서만 데이터 접근이 가능하기 때문에 인덱스에 상관없이 조회를 원하면 HashSet을 사용하면 된다!
+        Set<Integer> set = new HashSet<>();
+
+        for (int i : arr) {
+            int complement = target - i;
+            if (set.contains(complement)) {
+                return true;
+            } else {
+                set.add(i);
+            }
+        }
         return false;
     }
 }
