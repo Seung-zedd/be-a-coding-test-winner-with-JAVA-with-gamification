83aa8a7689e6d152c019d65e431713ae7f013335
michael Jo <csz0209@gmail.com>
2026-03-15 18:15:12 +0900

    👾일일 퀘스트 완료: 기능 개발 4회 던전 정복(S랭크)
    desc: 코테 문제가 CS지식 및 프로젝트 로직과 연관되는 영광의 순간!⭐

--- src/queue/FunDevSolution.java
+++ src/queue/FunDevSolution.java
@@ -25,7 +25,37 @@ import java.util.*;
 
 public class FunDevSolution {
     public int[] solution(int[] progresses, int[] speeds) {
+        // 순서를 위해 덱을 기반으로 하는 큐 생성
+        Deque<Integer> q = new ArrayDeque<>();
 
-        return new int[] {};
+        // 남은 개발 일짜 계산
+        for (int i = 0; i < progresses.length; i++) {
+            int day = (100 - progresses[i]) / speeds[i];
+            int remain = (100 - progresses[i]) % speeds[i];
+
+            // 나누어 떨어지지 않으면 day 1 증가
+            if (remain != 0) {
+                day++;
+            }
+
+            q.offer(day);
+        }
+        
+        // 배포 날짜 계산 시작
+        List<Integer> answer = new ArrayList<>();
+        while (!q.isEmpty()) {
+            int feature = 0;
+            int maxDate = q.poll();
+            feature++; // 꺼냈으면 기능 1 추가
+            // 단조 스택의 맹공 스킬을 활용해서 계속해서 잡아먹는다
+            while (!q.isEmpty() && (maxDate >= q.peek())) {
+                q.poll();
+                feature++; 
+            }
+
+            answer.add(feature);
+        }
+
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
