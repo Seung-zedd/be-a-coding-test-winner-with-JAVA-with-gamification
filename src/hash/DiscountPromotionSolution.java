fdca1273f273e0d2b84b24b406adaaed14c05d31
michael Jo <csz0209@gmail.com>
2026-03-16 13:27:42 +0900

    👾일일 퀘스트 완료: 오픈채팅방 3회 던전 정복(S랭크)

--- src/hash/DiscountPromotionSolution.java
+++ src/hash/DiscountPromotionSolution.java
@@ -35,43 +35,7 @@ XYZ 마트에서는 회원을 대상으로 매일 한 가지 제품을 할인하
 
 public class DiscountPromotionSolution {
     public int solution(String[] want, int[] number, String[] discount) {
-        // [복습 1회차] 할인 행사
-        int count = 0;
 
-        // 단일 슬라이딩 윈도우 기법으로 문제 돌파
-        Map<String, Integer> wantMap = new HashMap<>();
-        // 제품, 수량 삽입(최대 길이 10)
-        for (int i = 0; i < want.length; i++) {
-            wantMap.put(want[i], number[i]);
-        }
-
-        // discount 슬라이딩 윈도우 초기 세팅
-        Map<String, Integer> windowMap = new HashMap<>();
-        for (int i = 0; i < 10; i++) {
-            windowMap.merge(discount[i], 1, Integer::sum);
-        }
-        // 처음 1~10일차 비교
-        if (wantMap.equals(windowMap)) {
-            count++;
-        }
-
-        // 슬라이딩 윈도우 시작
-        for (int i = 10; i < discount.length; i++) {
-            String outItem = discount[i - 10];
-
-            // A) 나가는 품목
-            //? 수량이 1이면 연산 결과가 null이 되는 순간 해당 키를 즉시 remove, 그렇지 않으면 수량 1을 감소
-            windowMap.computeIfPresent(outItem, (k, v) -> v == 1 ? null : v - 1);
-
-            // B) 들어가는 품목
-            String inItem = discount[i];
-            windowMap.merge(inItem, 1, Integer::sum);
-            // C) 검증
-            if (wantMap.equals(windowMap)) {
-                count++;
-            }
-        }
-
-        return count;
+        return 0;
     }
 }
--- src/hash/OpenChatSolution.java
+++ src/hash/OpenChatSolution.java
@@ -65,22 +65,23 @@ import java.util.*;
 
 public class OpenChatSolution {
     public String[] solution(String[] record) {
-        // record를 기반으로 해시맵에 최종 닉네임을 저장한다.
-        // 공백을 기준으로 command, userid, nickname인데 해시맵에는 <userid, nickname>만 저장
+        // 1. 최종 닉네임을 반영하기 위해 해시맵에 저장
         Map<String, String> userMap = new HashMap<>();
+
+        // 1-pass: 최종 닉네임만 추적
         for (int i = 0; i < record.length; i++) {
             String[] msg = record[i].split(" ");
-            // ! Leave는 username까지만 있으므로, msg의 길이가 3이상일 때만 파싱하고 맵에 저장
+
+            // 닉네임이 있을 때만 해시맵에 저장
             if (msg.length > 2) {
                 String userid = msg[1];
                 String nickname = msg[2];
+
                 userMap.put(userid, nickname);
             }
         }
 
-        // 완성된 해시맵을 기준으로 record를 순회하면서 메시지를 조립
-        // 이때, "+" 연산자로 조립이 가능, 왜냐하면 해시맵의 조회는 O(1)만 걸리기 때문
-        // ? Change는 리턴 메시지에 포함되지 않기 때문에 record.length와 일치하지 않아서 가변 리스트 사용
+        // 2-pass: record의 파라미터를 추출해서 메시지와 조립
         List<String> answer = new ArrayList<>();
         for (int i = 0; i < record.length; i++) {
             String[] msg = record[i].split(" ");
