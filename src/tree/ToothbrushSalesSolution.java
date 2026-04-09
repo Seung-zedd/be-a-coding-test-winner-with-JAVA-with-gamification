d021738ad748c6ec2522f7071be5e915d566c7f6
michael Jo <csz0209@gmail.com>
2026-04-09 22:30:00 +0900

    ♻️refactor: 기존의 while문을 재귀함수로 리팩토링해봤음.
    desc:
    while문 조건 -> base condition(while문의 여사건)
    x -> 그대로 x를 적용
    부모 노드로 위임 -> distribute(referMap.getOrDefault(person, "-"), referMoney)와 같이 파라미터를 부모 노드로 갱신

--- src/tree/ToothbrushSalesSolution.java
+++ src/tree/ToothbrushSalesSolution.java
@@ -104,18 +104,8 @@ public class ToothbrushSalesSolution {
 
             // referral이 "-"가 아니고 curMoney가 0원이 아닐 때까지 while문 반복(pruning 효과)
             // while문 안에서 현재 상태를 계속해서 상향 전파해야 하기 때문에 cur 변수를 그대로 사용
-            //? curPerson이 center가 아니고 남은 금액이 0원을 초과할 때만 상향 전파를 수행
-            while (!curPerson.equals("-") && curMoney > 0) {
-                int referMoney = curMoney / 10;
-                int myMoney = curMoney - referMoney;
-
-                // 내 돈을 먼저 챙김
-                moneyMap.merge(curPerson, myMoney, Integer::sum);
-
-                // 나머지는 부모 노드로 위임
-                curMoney = referMoney;
-                curPerson = referMap.getOrDefault(curPerson, "-"); // center도 처리
-            }
+            // ? curPerson이 center가 아니고 남은 금액이 0원을 초과할 때만 상향 전파를 수행
+            distribute(curPerson, curMoney, moneyMap, referMap);
         }
 
         // enroll을 순회하면서 이익금 answer에 저장
@@ -125,5 +115,24 @@ public class ToothbrushSalesSolution {
         }
 
         return answer;
+    }
+
+    private static void distribute(String person, int money, Map<String, Integer> moneyMap, Map<String, String> referMap) {
+        // base condition
+        // 상향 전파를 하지 않을 조건: 현재 사람이 center거나 돈이 0원이면 절사해서 배당금을 전달하지 않아도 됨
+        //? while문의 여집합
+        if (person.equals("-") || money == 0) {
+            return;
+        }
+
+        // 상향 전파 시작
+        int referMoney = money / 10;
+        int myMoney = money - referMoney;
+
+        // 내 돈을 먼저 챙김
+        moneyMap.merge(person, myMoney, Integer::sum);
+
+        // 나머지는 부모 노드로 위임
+        distribute(referMap.getOrDefault(person, "-"), referMoney, moneyMap, referMap);
     }
 }
